a0924eacc3864ee63247f800913ac711f21491ec
xikl <1392936858@qq.com>
2019-01-22 00:51:53 +0800

    关键字的学习

---
+++ src/main/scala/com/ximo/scala/cource04/scopeA/package.scala
@@ -0,0 +1,43 @@
+package com.ximo.scala.cource04
+
+/**
+  *
+  * @author xikl
+  * @date 2019/1/21
+  */
+package object scopeA {
+  class PublicClass1 {
+
+    val publicField = 1
+
+    class Nested {
+      val nestedField = 1
+    }
+
+    val nested = new Nested
+
+  }
+
+  class PublicClass2 extends PublicClass1 {
+
+    val field2 = publicField + 1
+    val nField2 = new Nested().nestedField
+
+  }
+
+}
+
+package object scopeB{
+
+  class PublicClass1B extends scopeA.PublicClass1{
+
+      class UsingClass(val publicClass: scopeA.PublicClass1){
+
+        def method = " UsingClass: " +
+        " filed " + publicClass.publicField +
+        " nested field " + publicClass.nested.nestedField
+      }
+
+  }
+
+}
