451ff285e472340134bfd3e966e0b9070b8a34c8
xikl <1392936858@qq.com>
2019-01-26 21:38:52 +0800

    伴生类 和 对象

---
+++ src/main/scala/com/ximo/scala/cource04/ApplyApp.scala
@@ -0,0 +1,22 @@
+package com.ximo.scala.cource04
+
+/**
+  *
+  * @author xikl
+  * @date 2019/1/26
+  */
+object ApplyApp {
+
+}
+
+// 伴生类
+class ApplyClass{
+
+
+}
+
+// 伴生对象
+object ApplyClass{
+
+
+}
