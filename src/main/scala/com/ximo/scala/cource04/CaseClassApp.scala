1c9c49143e687da05e4c999da6956e81d8f1e44f
xikl <1392936858@qq.com>
2019-01-26 23:48:03 +0800

    case class 不用new 直接用

---
+++ src/main/scala/com/ximo/scala/cource04/CaseClassApp.scala
@@ -0,0 +1,20 @@
+package com.ximo.scala.cource04
+
+/**
+  *
+  * @author xikl
+  * @date 2019/1/26
+  */
+object CaseClassApp {
+
+  def main(args: Array[String]): Unit = {
+    println(Dog("zwz").name)
+  }
+
+}
+
+// 不用new 直接用 通常用在模式匹配中
+case class Dog(name:String){
+
+
+}
