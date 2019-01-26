6436c9b6aacde5518eb7f6da474ff22b3d15dbfb
xikl <1392936858@qq.com>
2019-01-26 21:55:15 +0800

    apply 通常返回一个 class对象

--- src/main/scala/com/ximo/scala/cource04/ApplyApp.scala
+++ src/main/scala/com/ximo/scala/cource04/ApplyApp.scala
@@ -7,16 +7,47 @@ package com.ximo.scala.cource04
   */
 object ApplyApp {
 
+  def main(args: Array[String]): Unit = {
+//    for (i <- 1 to 10) {
+//      ApplyClass.incr
+//    }
+//    // 单例对象
+//    println(ApplyClass.count)
+
+    // 会执行 object中的apply方法
+    val b = ApplyClass()
+
+
+    println("-----------------")
+    val c = new ApplyClass()
+    c()
+
+    // 类名() ==> object.apply
+    // 对象() ==> class.apply
+  }
+
 }
 
 // 伴生类
 class ApplyClass{
-
+  def apply() = println("class apply")
 
 }
 
 // 伴生对象
 object ApplyClass{
 
+  println("单例对象 enter。。。")
+
+  var count = 0
+
+  def incr = count = count + 1
+
+  def apply() = {
+    println("object apply")
+    new ApplyClass
+  }
+
+  println("单例对象 out。。。")
 
 }
