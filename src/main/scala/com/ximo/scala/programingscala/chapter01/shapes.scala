e976d2eb1fa41776c93e1a7d73ccbad78d351232
朱文赵 <zwz@servyou.com.cn>
2019-04-15 09:07:56 +0800

    去除注释

--- src/main/scala/com/ximo/scala/programingscala/chapter01/shapes.scala
+++ src/main/scala/com/ximo/scala/programingscala/chapter01/shapes.scala
@@ -24,45 +24,45 @@ case class Rectangle(lowerLeft: Point, height: Double, width: Double) extends Sh
 /** 三角形 */
 case class Triangle(point1: Point, point2: Point, point3: Point) extends Shape
 
-///**
-//  *
-//  *
-//  * @author 朱文赵
-//  * @date 2019/3/8 16:44
-//  */
-//object shapes {
-//
-//
-//  """
-//    |case 关键字的另一个特征便是让编译器自动为我们生成许多
-//    |方法，其中包括了类似于 Java 语言中 String 、 equals 和 hashCode 方法。每个点显示的输
-//    |出信息，如 Point(2.0,0.0) ，其实是 toString 方法的输出
-//  """.stripMargin
-//
-//
-//  def main(args: Array[String]): Unit = {
-//    val p00 = Point()
-//    //    val p001 = new Point()
-//
-//    val p20 = Point(2.0)
-//    println(p00 == p20)
-//
-//    val p20b = Point(2.0)
-//
-//    println(p20 == p20b)
-//
-//    val p02 = Point(y = 3.0)
-//
-//    // case class 会自动生成 equals方法进行比较 与 java 不同的操作
-//    // java 是= 比较引用 equals才是比较类是否相等
-//
-//    """
-//      |伴生类
-//      |case class 会自动生成伴生类
-//    """.stripMargin
-//
-//    val p30 = Point.apply(2.0, 3.0)
-//    val p30b = Point(2.0, 3.0)
-//
-//
-//  }
+/**
+  *
+  *
+  * @author 朱文赵
+  * @date 2019/3/8 16:44
+  */
+object shapes {
+
+  """
+    |case 关键字的另一个特征便是让编译器自动为我们生成许多
+    |方法，其中包括了类似于 Java 语言中 String 、 equals 和 hashCode 方法。每个点显示的输
+    |出信息，如 Point(2.0,0.0) ，其实是 toString 方法的输出
+  """.stripMargin
+
+
+  def main(args: Array[String]): Unit = {
+    val p00 = Point()
+    //    val p001 = new Point()
+
+    val p20 = Point(2.0)
+    println(p00 == p20)
+
+    val p20b = Point(2.0)
+
+    println(p20 == p20b)
+
+    val p02 = Point(y = 3.0)
+
+    // case class 会自动生成 equals方法进行比较 与 java 不同的操作
+    // java 是= 比较引用 equals才是比较类是否相等
+
+    """
+      |伴生类
+      |case class 会自动生成伴生类
+    """.stripMargin
+
+    val p30 = Point.apply(2.0, 3.0)
+    val p30b = Point(2.0, 3.0)
+
+
+  }
+}
