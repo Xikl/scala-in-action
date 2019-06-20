ef12c85e4ecc13cb890f817185930cde5be77a38
朱文赵 <1392936858@qq.com>
2019-06-21 00:24:31 +0800

    真正的解决办法

--- src/main/scala/com/ximo/scala/TestFlinkStreaming.scala
+++ src/main/scala/com/ximo/scala/TestFlinkStreaming.scala
@@ -1,7 +1,7 @@
 package com.ximo.scala
 
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 
 /**
   *
@@ -11,19 +11,28 @@ import org.apache.flink.streaming.api.scala._
   */
 object TestFlinkStreaming {
 
+  /**
+    * Error:(19, 37) could not find implicit value for evidence parameter of type org.apache.flink.api.common.typeinfo.TypeInformation[Long]
+    * val minusOne = iteration.map(_ - 1)
+    *
+    * @param args
+    */
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val someIntegers: DataStream[Long] = env.generateSequence(0, 1000)
 
-//    val iteratedStream = someIntegers.iterate(
-//      iteration => {
-//        val minusOne = iteration.map(_ - 1)
-//        val stillGreaterThanZero = minusOne.filter(_ > 0)
-//        val lessThanZero = minusOne.filter(_ <= 0)
-//        (stillGreaterThanZero, lessThanZero)
-//      }
+    // 添加隐式参数
+    implicit val typeInfo: TypeInformation[Long] = TypeInformation.of(classOf[Long])
 
-//    )
+    val iteratedStream = someIntegers.iterate(
+      iteration => {
+        val minusOne = iteration.map(_ - 1)
+        val stillGreaterThanZero = minusOne.filter(_ > 0)
+        val lessThanZero = minusOne.filter(_ <= 0)
+        (stillGreaterThanZero, lessThanZero)
+      }
+
+    )
   }
 
 
