9b7d59d14b139f33ae6680a97f86247fc2f9bfb0
朱文赵 <1392936858@qq.com>
2019-06-26 01:35:25 +0800

    模式匹配 List 和Seq的一些简单用法

---
+++ src/main/scala/com/ximo/scala/programingscala/chapter04/MatchSeqApp.scala
@@ -0,0 +1,72 @@
+package com.ximo.scala.programingscala.chapter04
+
+/**
+  *
+  *
+  * @author xikl
+  * @date 2019/6/26
+  */
+object MatchSeqApp {
+
+
+  val nonEmptySeq: Seq[Int] = Seq(1, 2, 3, 4)
+  val emptySeq = Seq.empty[Int]
+
+  val nonEmptyList = List(1, 2, 3)
+  val emptyList = Nil
+
+  val nonEmptyVector = Vector(1, 2, 3)
+  val emptyVector = Vector.empty[Int]
+
+  // 优秀的map创建方式
+  val nonEmptyMap = Map("one" -> 1, "tow" -> 2, "three" -> 3)
+  val emptyMap = Map.empty[String, Int]
+
+  /**
+    * 递归调用传入一个序列 然后返回字符串
+    * 相当于遍历
+    * 1 +: 2 +: 3 +: 4 +: 5 +: Nil
+    * Nil
+    * 1 +: 2 +: 3 +: 4 +: 5 +: Nil
+    * Nil
+    * 1 +: 2 +: 3 +: 4 +: 5 +: Nil
+    * Nil
+    * (one,1) +: (two,2) +: (three,3) +: Nil
+    * Nil
+    *
+    * 我们只用了两个 case 子句和递归就处理了序列。这暗示了所有序列的基础特性：序列要么
+    * 为空，要么非空。这听起来很老套，但一旦理解了这一点，你就会多一个基于“分治”法
+    * 的工具
+    *
+    * @param seq
+    * @tparam T
+    * @return
+    */
+  def seqToString[T](seq: Seq[T]): String = seq match {
+    // 头部 加上剩下的元素（尾部）
+    // 分别提取变量
+    case head +: tail => s"$head +: ${seqToString(tail)}"
+    case Nil => "Nil"
+  }
+
+  // 注意这里将map转化为 seq
+  for {
+    seq <- Seq(
+      nonEmptySeq, emptySeq, nonEmptyList, emptyList,
+      nonEmptyVector, emptyVector, nonEmptyMap.toSeq, emptyMap.toSeq
+    )
+  } println(seqToString(seq))
+
+  val nonEmptyList2 = List(1, 2, 3, 4)
+  val emptyList2 = Nil
+
+  def listToString[T](list: List[T]): String = {
+    case head :: tail => s"($head :: ${listToString(tail)})"
+    case Nil => "(Nil)"
+  }
+
+  // 用 :: 代替 +:
+  for (list <- List(nonEmptyList2, emptyList2)) {
+      println(listToString(list))
+  }
+}
