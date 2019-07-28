a346135a8e02d374094eaffcc9093caa61e86dce
朱文赵 <1392936858@qq.com>
2019-07-28 22:23:32 +0800

    利用java来写阶乘

---
+++ src/main/scala/com/ximo/scala/programingscala/chapter06/JavaFactorial.java
@@ -0,0 +1,68 @@
+package com.ximo.scala.programingscala.chapter06;
+
+import java.util.stream.LongStream;
+
+/**
+ * @author xikl
+ * @date 2019/7/28
+ */
+public class JavaFactorial {
+
+    /**
+     * 0的阶乘也是1
+     * 这里忽略负数的情况
+     *
+     * @param n
+     * @return
+     */
+    public static long factorial(long n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        long result = 1;
+
+        for (long j = 2; j <= n; j++) {
+            result *= j;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LongStream.rangeClosed(1, 10)
+                .map(JavaFactorial::factorial)
+                .forEach(System.out::println);
+
+    }
+
+    /**
+     * 不太好的递归
+     *
+     * @param n
+     * @return
+     */
+    public static long factorialRec(long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return n * factorialRec(n - 1);
+    }
+
+
+    public static long bestFactorialRec(long n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return bestFactorialRec(n, 1);
+    }
+
+    public static long bestFactorialRec(long n, long acc) {
+        if (n == 0) {
+            return acc;
+        }
+
+        return bestFactorialRec(n - 1, n * acc);
+    }
+
+}
