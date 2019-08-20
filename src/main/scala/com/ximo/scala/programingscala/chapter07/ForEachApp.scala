7406da1ac176353e8a80d75d907a652affb6180f
朱文赵 <1392936858@qq.com>
2019-08-20 22:29:12 +0800

    for推导

--- src/main/scala/com/ximo/scala/programingscala/chapter07/ForEachApp.scala
+++ src/main/scala/com/ximo/scala/programingscala/chapter07/ForEachApp.scala
@@ -21,8 +21,18 @@ object ForEachApp extends App {
     s <- states
     // 每个char
     c <- s
+    // 只要小写的
+    if c.isLower
   } yield s"$c - ${c.toUpper}"
 
   states flatMap (_.toSeq map (c => s"$c-${c.toUpper}"))
 
+  states flatMap(_.toSeq withFilter(_.isLower) map(c => s"$c - ${c.toUpper}"))
+
+  for {
+    s <- states
+    c <- s
+    if c.isLower
+    c2 = s"$c - ${c.toUpper}"
+  } yield c2
 }
